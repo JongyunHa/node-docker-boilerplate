1af38132719ac559fd58123715b11bd72abeff98
JongyunHa <gkwhddbs77@knou.ac.kr>
2021-06-12 13:44:27 +0900

    Build docker react test code

--- react-docker-example/src/App.js
+++ react-docker-example/src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          react-docker
+          Learn react
         </a>
       </header>
     </div>
